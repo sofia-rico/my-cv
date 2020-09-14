2e26af304173d67edeb459ca2d2ca759dde95775
Sofía de Oliveira Rico <71217016+sofia-rico@users.noreply.github.com>
2020-09-14 12:25:58 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
-function getParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+function getParameterByName(seach) {
+  return window.location.search.indexOf(seach) > -1;
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -13,18 +7,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const myContainer = document.querySelector(".container");
   const resumeWrapper = document.querySelector(".resume-wrapper");
   const phoneInfo = document.querySelector(".phone-info");
-  if (getParameterByName("m") === "print") {
+  if (getParameterByName("m=print")) {
+    hideItems.forEach((item) => {
+      item.style.display = "none";
+    });
     myContainer.classList.remove("px-3");
     myContainer.classList.remove("px-lg-5");
     resumeWrapper.classList.remove("p-5");
     resumeWrapper.classList.remove("shadow-lg");
     resumeWrapper.classList.remove("resume-wrapper");
-    hideItems.forEach((item) => {
-      item.style.display = "none";
-    });
   }
 
-  if (getParameterByName("p") === "1") {
+  if (getParameterByName("p=1")) {
     phoneInfo.classList.remove("phone-info");
   }
 });
