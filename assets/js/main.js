0f091742427b338c3886e9cd3e8920b5dd6d28ed
Sofía de Oliveira Rico <71217016+sofia-rico@users.noreply.github.com>
2020-09-14 12:45:32 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const resumeWrapper = document.querySelector(".resume-wrapper");
   const phoneInfo = document.querySelector(".phone-info");
   if (getParameterByName("m=print")) {
-    hideItems.map(function (item) {
-      item.style.display = "none";
-    });
     myContainer.classList.remove("px-3");
     myContainer.classList.remove("px-lg-5");
     resumeWrapper.classList.remove("p-5");
     resumeWrapper.classList.remove("shadow-lg");
     resumeWrapper.classList.remove("resume-wrapper");
+    for (var x = 0; x < hideItems.length; x += 1) {
+      hideItems[x].style.display = "none";
+    }
   }
 
   if (getParameterByName("p=1")) {
