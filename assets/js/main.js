cdb116c169e1cc629cb9e2ef7cd3e5ac42e6f7dd
Sofía de Oliveira Rico <71217016+sofia-rico@users.noreply.github.com>
2020-09-14 11:51:49 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,16 @@ function getParameterByName(name, url) {
 document.addEventListener("DOMContentLoaded", function (event) {
   const myContainer = document.querySelector(".container");
   const resumeWrapper = document.querySelector(".resume-wrapper");
+  const phoneInfo = document.querySelector(".phone-info");
   if (getParameterByName("m") === "print") {
     myContainer.classList.remove("px-3");
     myContainer.classList.remove("px-lg-5");
     resumeWrapper.classList.remove("p-5");
     resumeWrapper.classList.remove("shadow-lg");
     resumeWrapper.classList.remove("resume-wrapper");
+  }
+
+  if (getParameterByName("p") === "1") {
+    phoneInfo.classList.remove("phone-info");
   }
 });
